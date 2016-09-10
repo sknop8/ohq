6d3e9175ed0217cdb681c2e48172c356a0debc05
Dorothy Chang <dorothyichang@gmail.com>
2016-09-10 12:42:48 -0400

    merged bg js files

--- js/background.js
+++ js/background.js
@@ -1,5 +1,10 @@
+require(['https://cdn.firebase.com/js/client/2.4.2/firebase.js'], function (firebase) {});
+
  var queue = [];
 $(document).ready(function () {
+	console.log("document ready");
+	ref = new Firebase("https://queue-26963.firebaseio.com/");
+
   // queue = [];
 });
 
