1b4dce8ab8c4647fa1f207c32632fefd8d7ee14c
Dorothy Chang <dorothyichang@gmail.com>
2016-09-10 13:11:55 -0400

    fb

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,13 @@
 require(['https://cdn.firebase.com/js/client/2.4.2/firebase.js'], function (firebase) {});
 
-var ref = new Firebase("https://queue-26963.firebaseio.com/");
-
-var temp = ref.child("filler");
+var temp = rootRef.child("filler");
 
 $(document).ready(function () {
 	temp.set({title: "", text: ""});
 	console.log("temp set");
 });
 
-ref.limitToLast(50).on("value", function (snapshot) {
+rootRef.limitToLast(50).on("value", function (snapshot) {
   var data = snapshot.val();
   var title = data.title;
   var text = data.text;
@@ -20,7 +18,7 @@ ref.limitToLast(50).on("value", function (snapshot) {
   $("#blogposts").prepend(textElement);
 });
 
-ref.limitToLast(50).on("child_added", function (snapshot) {
+rootRef.limitToLast(50).on("child_added", function (snapshot) {
   var data = snapshot.val();
   var title = data.title;
   var text = data.text;
@@ -29,4 +27,19 @@ ref.limitToLast(50).on("child_added", function (snapshot) {
   titleElement.text(title);
   textElement.text(text).prepend(titleElement);
   $("#blogposts").prepend(textElement);
+});
+
+var studentInput = $("#issueForm");
+var textField = $("#textinput");
+
+$("#issueForm").on("submit", function (e) {
+  var title = titleField.val();
+  var text = textField.val();
+  ref.push({title: title, text: text});
+  titleField.val('');
+  textField.val('');
+  $("#logincontainer")[0].style.visibility="hidden";
+  $("#postcontainer")[0].style.visibility="visible";
+  e.preventDefault();
+  $("#successmessage")[0].style.visibility="visible";
 });
