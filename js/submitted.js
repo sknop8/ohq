705bd819d6dad856abfd9bc1559968ab83e1efff
Dorothy Chang <dorothyichang@gmail.com>
2016-09-10 23:30:30 -0400

    push

--- js/submitted.js
+++ js/submitted.js
@@ -1,8 +1,9 @@
 require(['https://cdn.firebase.com/js/client/2.4.2/firebase.js'], function (firebase) {});
-var count = 0;
+var count = 2;
 rootRef.child("students").once("value", function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
-      count++;
+      count=count+1;
       console.log("doing");
+    });
+    $("#peopleInFront").html(count);
 });
-$("#peopleInFront").html(count);
